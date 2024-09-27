5fb5004d5efe2217b19652a5448821276b9b941f
Mario Picasso <mariopicassodev@gmail.com>
2024-09-27 15:09:46 -0300

    REF: add to deploy local script file managemet for data

--- contracts/contracts/Airdrop.sol
+++ contracts/contracts/Airdrop.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
@@ -10,16 +9,17 @@ contract Airdrop is Ownable {
     mapping(address => uint256) public withdrawnAmount;
     IERC20 public token;
 
+    // Note: Ownable to then pause and resume the contract
     constructor(bytes32 _merkleRoot, address _tokenAddress) Ownable(msg.sender) {
         merkleRoot = _merkleRoot;
         token = IERC20(_tokenAddress);
     }
 
     function claim(bytes32[] calldata merkleProof, uint256 amount) public {
-        
+
         // Verify the Merkle proof
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(msg.sender, amount))));
-        require(MerkleProof.verify(merkleProof, merkleRoot, leaf), "Invalid Merkle proof");
+        require(verify(merkleProof, merkleRoot, leaf), "Invalid Merkle proof");
 
         // Calculate remaining amount to claim
         uint256 remainingAmount = amount - withdrawnAmount[msg.sender];
@@ -39,4 +39,25 @@ contract Airdrop is Ownable {
 
     // Fallback function to receive funds
     receive() external payable {}
+
+    // Check proof against merkle
+    function verify(bytes32[] memory proof, bytes32 root, bytes32 leaf) internal pure returns (bool) {
+        return processProof(proof, leaf) == root;
+    }
+
+    // Process the proof
+    function processProof(bytes32[] memory proof, bytes32 leaf) internal pure returns (bytes32) {
+        bytes32 computedHash = leaf;
+        for (uint256 i = 0; i < proof.length; i++) {
+            computedHash = _hashPair(computedHash, proof[i]);
+        }
+        return computedHash;
+    }
+
+    // Hash a pair of nodes in sorted order
+    function _hashPair(bytes32 a, bytes32 b) private pure returns (bytes32) {
+        return a < b ? keccak256(abi.encode(a, b)) : keccak256(abi.encode(b, a));
+    }
+
+
 }
--- contracts/scripts/deployLocal.js
+++ contracts/scripts/deployLocal.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+const fs = require('fs');
 const { ethers } = require('hardhat');
 const { StandardMerkleTree } = require("@openzeppelin/merkle-tree");
+const whitelist = require('../data/test-local-whitelist.json');
 
 async function main() {
     const provider = new ethers.getDefaultProvider("http://127.0.0.1:8545");
@@ -20,18 +22,10 @@ async function main() {
     console.log('Owner address:', wallet.address);
     console.log('Token balance:', await token.balanceOf(wallet.address));
 
-    // Build the merkle tree with a small whitelist
-    const whitelist_values = [
-        [wallet.address, "500"],
-        ["0x70997970C51812dc3A010C7d01b50e0d17dc79C8", "200"],
-        ["0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC", "100"],
-        ["0x90F79bf6EB2c4f870365E785982E1f101E93b906", "100"],
-        ["0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65", "50"],
-        ["0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc", "50"]
-    ];
-    const tree = StandardMerkleTree.of(whitelist_values, ["address", "uint256"]);
+    const tree = StandardMerkleTree.of(whitelist, ["address", "uint256"]);
     const merkleRoot = tree.root;
     console.log('Merkle Root:', merkleRoot);
+    fs.writeFileSync("./data/local-whitelist-tree.json", JSON.stringify(tree.dump()));
 
     // Deploy the Airdrop contract
     const Airdrop = await ethers.getContractFactory("Airdrop", wallet);
